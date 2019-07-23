c9deff632e54b6a33fe6a0f99844e92e93919860
gr4vity0 <jobrazil@gmail.com>
2019-07-23 10:20:14 +0200

    quelques idees pour le site

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
+
+// var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+// if (viewportWidth < 640){
+// 	for (let i = 0; i < navLink.length; i++) {
+// 		var node = document.createElement('i');
+//     node.className = 'fa fa-arrow-left ml-3';
+//         navLink[i].appendChild(node);
+//         console.log(navLink[i]);
+// 	}
+// } else{}
+// let navLinkBase = document.getElementsByClassName('nav-link');
+// let navLink = Array.from(navLinkBase);
+
+
+// if (screen.width < 640){
+// 	for (let i = 0; i < navLink.length; i++) {
+// 		var node = document.createElement('i');
+//     node.className = 'fa fa-arrow-left ml-3';
+//         navLink[i].appendChild(node);
+//         console.log(navLink[i]);
+// 	}
+// } else{}
+
